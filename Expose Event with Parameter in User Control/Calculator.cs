3aa1e4d749755a209fc99aedb7964b48ef7bb7fe
bashar.hazbar <hbashark99@gmail.com>
2024-08-20 15:57:28 +0700

    Expose Event with Parameter in User Control

---
+++ Expose Event with Parameter in User Control/Calculator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Expose_Event_with_Parameter_in_User_Control
+{
+    public partial class Calculator : UserControl
+    {
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        public event Action<int> OnCalculated;
+
+        protected virtual void Calculated(int Result)
+        {
+            Action<int> Calculated = OnCalculated;
+
+            if (Calculated != null)
+            {
+                Calculated(Result);
+            }
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            lbResult.Text = (int.Parse(textBox1.Text) + int.Parse(textBox2.Text)).ToString();
+            if (OnCalculated != null)
+            {
+                Calculated(int.Parse(lbResult.Text));
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        
+    }
+}
---
+++ Expose Event with Parameter in User Control/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Expose_Event_with_Parameter_in_User_Control
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calculator_OnCalculated(int obj)
+        {
+            MessageBox.Show("The Reslut of Calc is: " + obj);
+        }
+    }
+}
