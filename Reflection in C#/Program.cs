58254893ad7f90bdddf28f0c8737c422a236506a
bashar.hazbar <hbashark99@gmail.com>
2024-09-23 21:16:04 +0700

    The Reflection In C#

---
+++ Reflection in C#/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection_in_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Type type = typeof(string);
+
+            Console.WriteLine("Type Information"); 
+            Console.WriteLine("Full Name: " + type.FullName);
+            Console.WriteLine("Name: " + type.Name);
+            Console.WriteLine("Is Class: " + type.IsClass);
+            Console.WriteLine("");
+
+        }
+    }
+}
