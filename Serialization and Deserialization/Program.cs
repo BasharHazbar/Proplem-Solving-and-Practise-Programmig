f613285d8e86739be85fd9cd8bf89292b4c86708
bashar.hazbar <hbashark99@gmail.com>
2024-09-23 21:15:15 +0700

    Problem Solving Serialization and Deserialization In C#

---
+++ Serialization and Deserialization/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Serialization_and_Deserialization
+{
+
+
+
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            /// XML Serialization
+            Person person = new Person ("Bashar Abdulwahab", 23);
+            string XmlFileName = "Person1.xml";
+
+            clsSerializationAndDeserialization.SerializationToXml(person, XmlFileName);
+
+            Person DeserializationXmlToPerson = null;
+
+            DeserializationXmlToPerson = (Person)clsSerializationAndDeserialization.DeSerializationFromXml(DeserializationXmlToPerson, XmlFileName);
+
+            Console.WriteLine($"The Name is {DeserializationXmlToPerson.Name} and its age is {DeserializationXmlToPerson.Age}");
+
+
+            /////////////////////////////////////////////
+            /// Json Serialization
+
+            Person Person2 = new Person("Ali Saleh", 45);
+            string JsonFileName = "Person2.json";
+
+            clsSerializationAndDeserialization.SerializationToJson(Person2, JsonFileName);
+
+            Person DeserializationJsonToPerson = null;
+
+            DeserializationJsonToPerson = (Person)clsSerializationAndDeserialization.DeSerializationFromJson(DeserializationJsonToPerson, JsonFileName);
+
+            Console.WriteLine($"The Name is {DeserializationJsonToPerson.Name} and its age is {DeserializationJsonToPerson.Age}");
+
+
+            /////////////////////////////////////////////
+            /// Binary Serialization
+            /// 
+
+            Person Person3 = new Person("Mohammed Khaled", 100);
+            string BinaryFileName = "Person3.bin";
+
+            clsSerializationAndDeserialization.SerializationToBinary(Person3, BinaryFileName);
+
+            Person DeserializationBinaryToPerson = null;
+
+            DeserializationBinaryToPerson = (Person)clsSerializationAndDeserialization.DeSerializationFromBinary(DeserializationBinaryToPerson, BinaryFileName);
+
+            Console.WriteLine($"The Name is {DeserializationBinaryToPerson.Name} and its age is {DeserializationBinaryToPerson.Age}");
+
+
+
+
+        }
+    }
+    
+}
---
+++ Serialization and Deserialization/clsSerializationAndDeserialization.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Serialization_and_Deserialization
+{
+    [Serializable]
+    public class Person
+    {
+        public int Age { set; get; }
+        public string Name { set; get; }
+
+        public Person() { }
+
+        public Person(string name, int age)
+        {
+            Age = age;
+            Name = name;
+        }
+    }
+    public class clsSerializationAndDeserialization
+    {
+        // XML Serialization
+
+        private static XmlSerializer _XmlSerializer;
+        public static void SerializationToXml(object O , string XmlFileName)
+        {
+             _XmlSerializer = new XmlSerializer(O.GetType());
+
+            using (TextWriter writer = new StreamWriter(XmlFileName))
+            {
+                _XmlSerializer.Serialize(writer, O);
+            }
+        }
+
+        public static object DeSerializationFromXml(object O, string XmlFileName)
+        {
+            using (TextReader reader = new StreamReader(XmlFileName))
+            {
+                 O = _XmlSerializer.Deserialize(reader);
+
+                //Console.WriteLine($"The Name is {DeserializationPerson.Name} and its age is {DeserializationPerson.Age}");
+            }
+
+            return O;
+
+        }
+
+        //// Json Serialization
+
+        private static DataContractJsonSerializer _JsonSerializer;
+
+        public static void SerializationToJson(object O, string FileJsonName)
+        {
+            _JsonSerializer = new DataContractJsonSerializer(O.GetType());
+
+            using (MemoryStream Stream = new MemoryStream())
+            {
+                _JsonSerializer.WriteObject(Stream, O);
+
+                string Json = System.Text.Encoding.UTF8.GetString(Stream.ToArray());
+
+                File.WriteAllText(FileJsonName,Json);
+
+            }
+        }
+
+        public static object DeSerializationFromJson(object O, string FileJsonName)
+        {
+            using (FileStream stream = new FileStream(FileJsonName,FileMode.Open))
+            {
+                O = _JsonSerializer.ReadObject(stream);
+
+                //Console.WriteLine($"The Name is {DeserializationPerson.Name} and its age is {DeserializationPerson.Age}");
+            }
+
+            return O;
+
+        }
+
+        //// Binary Serialization
+        ///
+        private static BinaryFormatter _BinaryFormatter;
+
+        public static void SerializationToBinary(object O, string FileBinaryName)
+        {
+            _BinaryFormatter = new BinaryFormatter();
+
+            using (FileStream stream = new FileStream(FileBinaryName, FileMode.Create))
+            {
+                _BinaryFormatter.Serialize(stream,O);
+            }
+        }
+
+        public static object DeSerializationFromBinary(object O, string FileBinaryName)
+        {
+            using (FileStream stream = new FileStream(FileBinaryName, FileMode.Open))
+            {
+                O = _BinaryFormatter.Deserialize(stream);
+            }
+
+            return O;
+        }
+
+
+    }
+
+}
